473eea61fcc775d0c5a137b41d461f1d4736c8df
Nádia Nogues <nadianogues19@gmail.com>
2019-06-16 22:47:06 -0300

    Arquivos renomeados e adicionados no projeto

--- ProjetoABCdaLibra/app/src/main/java/com/example/projetoabcdalibra/tela_aprender.java
+++ ProjetoABCdaLibra/app/src/main/java/com/example/projetoabcdalibra/tela_aprender.java
@@ -1,16 +1,190 @@
 package com.example.projetoabcdalibra;
 
+import android.content.Intent;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.CompoundButton;
+import android.widget.Switch;
 
-public class tela_aprender extends AppCompatActivity {
+public class tela_aprender extends AppCompatActivity implements View.OnClickListener, CompoundButton.OnCheckedChangeListener
+{
+    Button btnA, btnB, btnC, btnD, btnE, btnF, btnG, btnH, btnI, btnJ, btnK, btnL, btnM;
+    Button btnN, btnO, btnP, btnQ, btnR, btnS, btnT, btnU, btnV, btnW, btnX, btnY, btnZ;
+    Switch chaveVogalConsoante;
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_tela_aprender);
         ActionBar title = getSupportActionBar();
         title.setTitle("Aprendendo o alfabeto");
+
+        btnA = (Button)findViewById(R.id.btnA);
+        btnA.setOnClickListener(this);
+        btnB = (Button)findViewById(R.id.btnB);
+        btnB.setOnClickListener(this);
+        btnC = (Button)findViewById(R.id.btnC);
+        btnC.setOnClickListener(this);
+        btnD = (Button)findViewById(R.id.btnD);
+        btnD.setOnClickListener(this);
+        btnE = (Button)findViewById(R.id.btnE);
+        btnE.setOnClickListener(this);
+        btnF = (Button)findViewById(R.id.btnF);
+        btnF.setOnClickListener(this);
+        btnG = (Button)findViewById(R.id.btnG);
+        btnG.setOnClickListener(this);
+        btnH = (Button)findViewById(R.id.btnH);
+        btnH.setOnClickListener(this);
+        btnI = (Button)findViewById(R.id.btnI);
+        btnI.setOnClickListener(this);
+        btnJ = (Button)findViewById(R.id.btnJ);
+        btnJ.setOnClickListener(this);
+        btnK = (Button)findViewById(R.id.btnK);
+        btnK.setOnClickListener(this);
+        btnL = (Button)findViewById(R.id.btnL);
+        btnL.setOnClickListener(this);
+        btnM = (Button)findViewById(R.id.btnM);
+        btnM.setOnClickListener(this);
+        btnN = (Button)findViewById(R.id.btnN);
+        btnN.setOnClickListener(this);
+        btnO = (Button)findViewById(R.id.btnO);
+        btnO.setOnClickListener(this);
+        btnP = (Button)findViewById(R.id.btnP);
+        btnP.setOnClickListener(this);
+        btnQ = (Button)findViewById(R.id.btnQ);
+        btnQ.setOnClickListener(this);
+        btnR = (Button)findViewById(R.id.btnR);
+        btnR.setOnClickListener(this);
+        btnS = (Button)findViewById(R.id.btnS);
+        btnS.setOnClickListener(this);
+        btnT = (Button)findViewById(R.id.btnT);
+        btnT.setOnClickListener(this);
+        btnU = (Button)findViewById(R.id.btnU);
+        btnU.setOnClickListener(this);
+        btnV = (Button)findViewById(R.id.btnV);
+        btnV.setOnClickListener(this);
+        btnW = (Button)findViewById(R.id.btnW);
+        btnW.setOnClickListener(this);
+        btnX = (Button)findViewById(R.id.btnX);
+        btnX.setOnClickListener(this);
+        btnY = (Button)findViewById(R.id.btnY);
+        btnY.setOnClickListener(this);
+        btnZ = (Button)findViewById(R.id.btnZ);
+        btnZ.setOnClickListener(this);
+
+        chaveVogalConsoante = (Switch)findViewById(R.id.chaveVogalConsoante);
+        chaveVogalConsoante.setOnCheckedChangeListener(this);
+        chaveVogalConsoante.setTextOn("");
+        chaveVogalConsoante.setTextOff("");
+
+
+        btnA.setEnabled(false);
+        btnE.setEnabled(false);
+        btnI.setEnabled(false);
+        btnO.setEnabled(false);
+        btnU.setEnabled(false);
+
+        btnB.setEnabled(true);
+        btnC.setEnabled(true);
+        btnD.setEnabled(true);
+        btnF.setEnabled(true);
+        btnG.setEnabled(true);
+        btnH.setEnabled(true);
+        btnJ.setEnabled(true);
+        btnK.setEnabled(true);
+        btnL.setEnabled(true);
+        btnM.setEnabled(true);
+        btnN.setEnabled(true);
+        btnP.setEnabled(true);
+        btnQ.setEnabled(true);
+        btnR.setEnabled(true);
+        btnS.setEnabled(true);
+        btnT.setEnabled(true);
+        btnV.setEnabled(true);
+        btnX.setEnabled(true);
+        btnW.setEnabled(true);
+        btnY.setEnabled(true);
+        btnZ.setEnabled(true);
+    }
+
+    @Override
+    public void onClick(View v)
+    {
+        Button btn = (Button)findViewById(v.getId());
+        Intent i = new Intent(this, tela_video.class);
+        Bundle b = new Bundle();
+        b.putString("letra", btn.getText().toString());
+        i.putExtras(b);
+        startActivity(i);
+    }
+
+    @Override
+    public void onCheckedChanged(CompoundButton buttonView, boolean isChecked)
+    {
+        if(isChecked)
+        {
+            btnA.setEnabled(true);
+            btnE.setEnabled(true);
+            btnI.setEnabled(true);
+            btnO.setEnabled(true);
+            btnU.setEnabled(true);
+
+            btnB.setEnabled(false);
+            btnC.setEnabled(false);
+            btnD.setEnabled(false);
+            btnF.setEnabled(false);
+            btnG.setEnabled(false);
+            btnH.setEnabled(false);
+            btnJ.setEnabled(false);
+            btnK.setEnabled(false);
+            btnL.setEnabled(false);
+            btnM.setEnabled(false);
+            btnN.setEnabled(false);
+            btnP.setEnabled(false);
+            btnQ.setEnabled(false);
+            btnR.setEnabled(false);
+            btnS.setEnabled(false);
+            btnT.setEnabled(false);
+            btnV.setEnabled(false);
+            btnX.setEnabled(false);
+            btnW.setEnabled(false);
+            btnY.setEnabled(false);
+            btnZ.setEnabled(false);
+
+        }
+        else
+        {
+            btnA.setEnabled(false);
+            btnE.setEnabled(false);
+            btnI.setEnabled(false);
+            btnO.setEnabled(false);
+            btnU.setEnabled(false);
+
+            btnB.setEnabled(true);
+            btnC.setEnabled(true);
+            btnD.setEnabled(true);
+            btnF.setEnabled(true);
+            btnG.setEnabled(true);
+            btnH.setEnabled(true);
+            btnJ.setEnabled(true);
+            btnK.setEnabled(true);
+            btnL.setEnabled(true);
+            btnM.setEnabled(true);
+            btnN.setEnabled(true);
+            btnP.setEnabled(true);
+            btnQ.setEnabled(true);
+            btnR.setEnabled(true);
+            btnS.setEnabled(true);
+            btnT.setEnabled(true);
+            btnV.setEnabled(true);
+            btnX.setEnabled(true);
+            btnW.setEnabled(true);
+            btnY.setEnabled(true);
+            btnZ.setEnabled(true);
+        }
     }
 }
--- ProjetoABCdaLibra/app/src/main/java/com/example/projetoabcdalibra/tela_video.java
+++ ProjetoABCdaLibra/app/src/main/java/com/example/projetoabcdalibra/tela_video.java
@@ -1,11 +1,17 @@
 package com.example.projetoabcdalibra;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.MediaController;
+import android.widget.VideoView;
 
-public class tela_video extends AppCompatActivity {
-
+public class tela_video extends AppCompatActivity
+{
+    private VideoView video;
     @Override
     protected void onCreate(Bundle savedInstanceState)
     {
@@ -14,5 +20,228 @@ public class tela_video extends AppCompatActivity {
 
         ActionBar title = getSupportActionBar();
         title.setTitle(" ");
+
+        Intent i = getIntent();
+        if(i != null)
+        {
+            Bundle caixa = new Bundle();
+            caixa = i.getExtras();
+
+            //testa se o bundle veio realmente
+            if(caixa != null)
+            {
+                if(caixa.getString("letra").toUpperCase().equals('A'))
+                {
+                    video = (VideoView)findViewById(R.id.video);
+                    video.setVideoURI(Uri.parse("android.resource://" + getPackageName() + "/" + R.raw.a));
+                    MediaController controle = new MediaController(this);
+                    video.setMediaController(controle);
+                    video.start();
+                }
+                else if(caixa.getString("letra").toUpperCase().equals('B'))
+                {
+                    video = (VideoView)findViewById(R.id.video);
+                    video.setVideoURI(Uri.parse("android.resource://" + getPackageName() + "/" + R.raw.b));
+                    MediaController controle = new MediaController(this);
+                    video.setMediaController(controle);
+                    video.start();
+                }
+                else if(caixa.getString("letra").toUpperCase().equals('C'))
+                {
+                    video = (VideoView)findViewById(R.id.video);
+                    video.setVideoURI(Uri.parse("android.resource://" + getPackageName() + "/" + R.raw.c));
+                    MediaController controle = new MediaController(this);
+                    video.setMediaController(controle);
+                    video.start();
+                }
+                else if(caixa.getString("letra").toUpperCase().equals('D'))
+                {
+                    video = (VideoView)findViewById(R.id.video);
+                    video.setVideoURI(Uri.parse("android.resource://" + getPackageName() + "/" + R.raw.d));
+                    MediaController controle = new MediaController(this);
+                    video.setMediaController(controle);
+                    video.start();
+                }
+                else if(caixa.getString("letra").toUpperCase().equals('E'))
+                {
+                    video = (VideoView)findViewById(R.id.video);
+                    video.setVideoURI(Uri.parse("android.resource://" + getPackageName() + "/" + R.raw.e));
+                    MediaController controle = new MediaController(this);
+                    video.setMediaController(controle);
+                    video.start();
+                }
+                else if(caixa.getString("letra").toUpperCase().equals('F'))
+                {
+                    video = (VideoView)findViewById(R.id.video);
+                    video.setVideoURI(Uri.parse("android.resource://" + getPackageName() + "/" + R.raw.f));
+                    MediaController controle = new MediaController(this);
+                    video.setMediaController(controle);
+                    video.start();
+                }
+                else if(caixa.getString("letra").toUpperCase().equals('G'))
+                {
+                    video = (VideoView)findViewById(R.id.video);
+                    video.setVideoURI(Uri.parse("android.resource://" + getPackageName() + "/" + R.raw.g));
+                    MediaController controle = new MediaController(this);
+                    video.setMediaController(controle);
+                    video.start();
+                }
+                else if(caixa.getString("letra").toUpperCase().equals('H'))
+                {
+                    video = (VideoView)findViewById(R.id.video);
+                    video.setVideoURI(Uri.parse("android.resource://" + getPackageName() + "/" + R.raw.h));
+                    MediaController controle = new MediaController(this);
+                    video.setMediaController(controle);
+                    video.start();
+                }
+                else if(caixa.getString("letra").toUpperCase().equals('I'))
+                {
+                    video = (VideoView)findViewById(R.id.video);
+                    video.setVideoURI(Uri.parse("android.resource://" + getPackageName() + "/" + R.raw.i));
+                    MediaController controle = new MediaController(this);
+                    video.setMediaController(controle);
+                    video.start();
+                }
+                else if(caixa.getString("letra").toUpperCase().equals('J'))
+                {
+                    video = (VideoView)findViewById(R.id.video);
+                    video.setVideoURI(Uri.parse("android.resource://" + getPackageName() + "/" + R.raw.j));
+                    MediaController controle = new MediaController(this);
+                    video.setMediaController(controle);
+                    video.start();
+                }else if(caixa.getString("letra").toUpperCase().equals('K'))
+                {
+                    video = (VideoView)findViewById(R.id.video);
+                    video.setVideoURI(Uri.parse("android.resource://" + getPackageName() + "/" + R.raw.k));
+                    MediaController controle = new MediaController(this);
+                    video.setMediaController(controle);
+                    video.start();
+                }
+                else if(caixa.getString("letra").toUpperCase().equals('L'))
+                {
+                    video = (VideoView)findViewById(R.id.video);
+                    video.setVideoURI(Uri.parse("android.resource://" + getPackageName() + "/" + R.raw.l));
+                    MediaController controle = new MediaController(this);
+                    video.setMediaController(controle);
+                    video.start();
+                }
+                else if(caixa.getString("letra").toUpperCase().equals('M'))
+                {
+                    video = (VideoView)findViewById(R.id.video);
+                    video.setVideoURI(Uri.parse("android.resource://" + getPackageName() + "/" + R.raw.m));
+                    MediaController controle = new MediaController(this);
+                    video.setMediaController(controle);
+                    video.start();
+                }
+                else if(caixa.getString("letra").toUpperCase().equals('N'))
+                {
+                    video = (VideoView)findViewById(R.id.video);
+                    video.setVideoURI(Uri.parse("android.resource://" + getPackageName() + "/" + R.raw.n));
+                    MediaController controle = new MediaController(this);
+                    video.setMediaController(controle);
+                    video.start();
+                }
+                else if(caixa.getString("letra").toUpperCase().equals('O'))
+                {
+                    video = (VideoView)findViewById(R.id.video);
+                    video.setVideoURI(Uri.parse("android.resource://" + getPackageName() + "/" + R.raw.o));
+                    MediaController controle = new MediaController(this);
+                    video.setMediaController(controle);
+                    video.start();
+                }
+                else if(caixa.getString("letra").toUpperCase().equals('P'))
+                {
+                    video = (VideoView)findViewById(R.id.video);
+                    video.setVideoURI(Uri.parse("android.resource://" + getPackageName() + "/" + R.raw.p));
+                    MediaController controle = new MediaController(this);
+                    video.setMediaController(controle);
+                    video.start();
+                }
+                else if(caixa.getString("letra").toUpperCase().equals('Q'))
+                {
+                    video = (VideoView)findViewById(R.id.video);
+                    video.setVideoURI(Uri.parse("android.resource://" + getPackageName() + "/" + R.raw.q));
+                    MediaController controle = new MediaController(this);
+                    video.setMediaController(controle);
+                    video.start();
+                }
+                else if(caixa.getString("letra").toUpperCase().equals('R'))
+                {
+                    video = (VideoView)findViewById(R.id.video);
+                    video.setVideoURI(Uri.parse("android.resource://" + getPackageName() + "/" + R.raw.r));
+                    MediaController controle = new MediaController(this);
+                    video.setMediaController(controle);
+                    video.start();
+                }
+                else if(caixa.getString("letra").toUpperCase().equals('S'))
+                {
+                    video = (VideoView)findViewById(R.id.video);
+                    video.setVideoURI(Uri.parse("android.resource://" + getPackageName() + "/" + R.raw.s));
+                    MediaController controle = new MediaController(this);
+                    video.setMediaController(controle);
+                    video.start();
+                }
+                else if(caixa.getString("letra").toUpperCase().equals('T'))
+                {
+                    video = (VideoView)findViewById(R.id.video);
+                    video.setVideoURI(Uri.parse("android.resource://" + getPackageName() + "/" + R.raw.t));
+                    MediaController controle = new MediaController(this);
+                    video.setMediaController(controle);
+                    video.start();
+                }
+                else if(caixa.getString("letra").toUpperCase().equals('U'))
+                {
+                    video = (VideoView)findViewById(R.id.video);
+                    video.setVideoURI(Uri.parse("android.resource://" + getPackageName() + "/" + R.raw.u));
+                    MediaController controle = new MediaController(this);
+                    video.setMediaController(controle);
+                    video.start();
+                }
+                else if(caixa.getString("letra").toUpperCase().equals('V'))
+                {
+                    video = (VideoView)findViewById(R.id.video);
+                    video.setVideoURI(Uri.parse("android.resource://" + getPackageName() + "/" + R.raw.v));
+                    MediaController controle = new MediaController(this);
+                    video.setMediaController(controle);
+                    video.start();
+                }
+                else if(caixa.getString("letra").toUpperCase().equals('W'))
+                {
+                    video = (VideoView)findViewById(R.id.video);
+                    video.setVideoURI(Uri.parse("android.resource://" + getPackageName() + "/" + R.raw.w));
+                    MediaController controle = new MediaController(this);
+                    video.setMediaController(controle);
+                    video.start();
+                }
+                else if(caixa.getString("letra").toUpperCase().equals('X'))
+                {
+                    video = (VideoView)findViewById(R.id.video);
+                    video.setVideoURI(Uri.parse("android.resource://" + getPackageName() + "/" + R.raw.x));
+                    MediaController controle = new MediaController(this);
+                    video.setMediaController(controle);
+                    video.start();
+                }
+                else if(caixa.getString("letra").toUpperCase().equals('Y'))
+                {
+                    video = (VideoView)findViewById(R.id.video);
+                    video.setVideoURI(Uri.parse("android.resource://" + getPackageName() + "/" + R.raw.y));
+                    MediaController controle = new MediaController(this);
+                    video.setMediaController(controle);
+                    video.start();
+                }
+                else if(caixa.getString("letra").toUpperCase().equals('Z'))
+                {
+                    video = (VideoView)findViewById(R.id.video);
+                    video.setVideoURI(Uri.parse("android.resource://" + getPackageName() + "/" + R.raw.z));
+                    MediaController controle = new MediaController(this);
+                    video.setMediaController(controle);
+                    video.start();
+                }
+
+            }
+        }
+
     }
+
+
 }
